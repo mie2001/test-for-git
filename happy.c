772fbd982a5601b6b0107d9d9bb3828100c47fa5
mie2001 <450921468@qq.com>
2021-01-20 19:03:37 +0800

    i am tired

---
+++ happy.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("git make me happy!\n");
+    return 0;
+}
--- hello.c
+++ hello.c
@@ -4,5 +4,6 @@ int main()
     printf("hello world!\n");
     printf("i am mie\n");
     printf("i am hpj\n");
+    printf("i am jay\n");
     return 0;
 }
